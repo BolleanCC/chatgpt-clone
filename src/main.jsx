6fc5db4415e8837fe0adcd34b6b2c597ed257c41
BolleanCC <songtao19980820@outlook.com>
2025-05-17 04:04:20 +0000

    May 16, 2025, 10:04 PM

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Link, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Link, RouterProvider, createBrowserRouter } from "react-router-dom";
+import Homepage from "./routes/homepage/Homepage.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: (
-      <div>
-        <h1>Hello World</h1>
-        <Link></Link>
-      </div>
-    ),
+    element: <Homepage />,
   },
   {
     path: "about",
-    element: <div>About</div>
+    element: <div>About</div>,
   },
 ]);
 
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}>
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
