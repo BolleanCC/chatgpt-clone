49f42bd9093cf06e21562b7240206d0ac482f11b
BolleanCC <songtao19980820@outlook.com>
2025-05-17 04:43:14 +0000

    May 16, 2025, 10:43 PM

--- src/layouts/rootLayout/RootLayout.jsx
+++ src/layouts/rootLayout/RootLayout.jsx
@@ -1,7 +1,20 @@
+import { Outlet } from "react-router-dom";
 import "./rootLayout.css";
 
 const RootLayout = () => {
-  return <div className="rootLayout">RootLayout</div>;
+  return (
+    <div className="rootLayout">
+      <header>
+        <Link>
+          <img src="/logo.png" alt="" />
+          <span>LAMA AI</span>
+        </Link>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
 };
 
 export default RootLayout;
